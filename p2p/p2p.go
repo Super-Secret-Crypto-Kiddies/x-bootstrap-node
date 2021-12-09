477e94ba4ec3efa6f4bc7856a2ea443dafee8a94
turtlebasket <turtlebasket@gmail.com>
2021-12-08 20:29:08 -0800

    p2p stuff

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -1,6 +1,8 @@
 package p2p
 
 import (
+	"fmt"
+	"time"
 	"x-bootstrap-node/xutil"
 
 	"github.com/perlin-network/noise"
@@ -9,12 +11,21 @@ import (
 
 var Node *noise.Node = nil
 
+const p2pPort = 9871
+
 func InitP2P() {
 
 	var err error = nil
-	if Node, err = noise.NewNode(noise.WithNodeBindPort(9871)); err != nil {
+	if Node, err = noise.NewNode(
+		noise.WithNodeBindPort(p2pPort),
+		// CHANGE THIS PLS
+		noise.WithNodeAddress(fmt.Sprintf("143.198.176.143:%d", p2pPort)),
+	); err != nil {
 		panic(err)
 	}
+
+	k := kademlia.New()
+	Node.Bind(k.Protocol())
 
 	Node.Handle(func(ctx noise.HandlerContext) error {
 
@@ -29,10 +40,15 @@ func InitP2P() {
 		return nil
 	})
 
-	k := kademlia.New()
-	Node.Bind(k.Protocol())
-
 	if err := Node.Listen(); err != nil {
 		panic(err)
 	}
+
+	go func() {
+		for {
+			fmt.Printf("Peers: %s\n\n", k.Discover())
+			time.Sleep(30 * time.Second)
+		}
+	}()
+
 }
